7fe2b1f205c6c7ecd284ee0fb04152566535e4d4
Anatoly Tayursky <serviceworker@yandex.ru>
2020-05-10 02:12:16 +0300

    Helmet added and cors enabled

--- server/dist/main.js
+++ server/dist/main.js
@@ -2,8 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const helmet = require("helmet");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use(helmet());
+    app.enableCors();
     await app.listen(process.env.PORT);
 }
 bootstrap();
